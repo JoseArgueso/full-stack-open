662067accdcf968e6ff8b69a4f9a784a14be2caa
JoseArgueso <jose.argueso@outlook.com>
2025-03-14 11:27:41 +0100

    2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,30 @@
 import { useState } from 'react'
 
 const App = () => {
+  
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const Person = ({person}) => {
     
     return(
-      <li key={person.name}>{person.name}</li>
+      <li key={person.name}>{person.name} {person.number}</li>
     )
   }
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber,
+      id: persons.length+1
     }
 
     if(persons.filter(person => person.name === newName).length>0){
@@ -24,15 +32,21 @@ const App = () => {
       return
     }
     
-
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
 
   return (
     <div>
@@ -42,6 +56,12 @@ const App = () => {
           name: <input 
                   value={newName}
                   onChange={handleNameChange}
+                />
+        </div>
+        <div>
+          number: <input 
+                  value={newNumber}
+                  onChange={handleNumberChange}
                 />
         </div>
         <div>
